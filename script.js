e3fa6d40acde8ce84c52170c8dc3c7aadef7d8a5
cola314 <ubj0314@naver.com>
2021-08-27 23:50:00 +0900

    사용자 이름 설정 기능 추가
    - 쿠키로 저장 및 로드
    - 설정 탭에서 변경시 다른 탭에 적용

--- script.js
+++ script.js
@@ -1,36 +1,55 @@
 $(document).ready(() => {
-  const tabs = $('#mainTabs > ul > li').get();
-  const tabPages = $('.tabPage').get();
+  const tabs = $('#mainTabs > ul > li').get()
+  const tabPages = $('.tabPage').get()
+  let userName;
 
-  init();
-
-  function init() {
-    tabs.forEach((tab, idx) => {
-      $(tab).click(() => {
-        setTabMenu(idx)
-      });
+  // 탭 설정
+  tabs.forEach((tab, idx) => {
+    $(tab).click(() => {
+      setTabMenu(idx)
     });
+  });
 
-    setTabMenu(0);
-  }
+  setTabMenu(3);
 
   function setTabMenu(idx) {
     tabs.forEach((tabToActive, activeIdx) => {
       if (idx == activeIdx) {
-        $(tabToActive).addClass('is-active');
+        $(tabToActive).addClass('is-active')
       } else {
-        $(tabToActive).removeClass('is-active');
+        $(tabToActive).removeClass('is-active')
       }
     });
 
     tabPages.forEach((pageToShow, pageIdx) => {
       if (idx == pageIdx) {
-        $(pageToShow).show();
+        $(pageToShow).show()
       } else {
-        $(pageToShow).hide();
+        $(pageToShow).hide()
       }
     });
   };
+
+  // 설정 탭
+  setUserName($.cookie('name') || '')
+  $('#nameSettingInput').val(userName)
+
+  $('#nameSettingBtn').click(() => {
+    const name = $('#nameSettingInput').val().trim()
+
+    if (name) {
+      setUserName(name)
+    }
+  });
+
+  function setUserName(name) {
+    userName = name
+    $.cookie('name', name, { expires: 90 })
+    $('.userNameTextBox').get().forEach(textBox => {
+      $(textBox).val(name)
+    })
+  }
+
 
 });
 
